b99632ed30513f45049209e7782ef47c1170438f
Carlos García <carlosgarciafm@gmail.com>
2021-12-23 20:27:31 -0600

    feat(c): add session support

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+from flask_session import Session
+from tempfile import mkdtemp
 from dbschema import User, Purchase, Payment, db
 
+# Application configuration.
 app = Flask(__name__)
+
+# Database configuration.
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///paymenttracker.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.app = app
 db.init_app(app)
 db.create_all()
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 
 @app.route("/")
 def index():
