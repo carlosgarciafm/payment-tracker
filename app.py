c9c585beaa122fde5a8a4bbb995f58a3c5e8b0cd
Carlos García <carlosgarciafm@gmail.com>
2021-12-23 18:03:56 -0600

    feat(m): programmatically create tables in db
    By default `db.create_all()` checks if the tables already exist in the
    database and omits the creation if they do.

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from dbschema import User, Purchase, Payment, db
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///paymenttracker.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.app = app
+db.init_app(app)
+db.create_all()
 
 @app.route("/")
 def index():
